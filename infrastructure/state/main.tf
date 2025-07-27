543f9c973b0415251919f11ef51cdcfa03cd87a4
Ryan Maber <ryan@ryanmaber.com>
2025-07-27 01:30:14 +0100

    Rename state bucket

--- infrastructure/state/main.tf
+++ infrastructure/state/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "aws-cognito-srp-tf-state"
+  bucket = "aws-cognito-srp-terraform-state"
 
   lifecycle {
     prevent_destroy = true
